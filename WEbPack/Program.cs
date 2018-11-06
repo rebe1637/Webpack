a5b4bb939ba50f4bf8a1ef79cc84201cbbc7411b
rebe1637 <33025849+rebe1637@users.noreply.github.com>
2018-11-06 11:19:26 +0100

    Add project files.

---
+++ WEbPack/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WEbPack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
